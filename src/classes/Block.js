25c1816269af8d6aa4acb87e30e0095367770f3f
AdrienGentizon <adrien.gentizon@gmail.com>
2020-05-16 08:47:18 +0200

    Refactoring

--- src/classes/Block.js
+++ src/classes/Block.js
@@ -1,13 +1,31 @@
+const pixi = require('pixi.js');
 const Vector = require('./Vector');
 
 class Block {
-  constructor(sprite, game, value) {
-    this.sprite = sprite;
+  constructor(sprite, game, value = -1) {
+    this._sprite = sprite;
     this._game = game;
     this._value = value;
-    this.isStopped = false;
+    this._isStopped = false;
     this._prevPosition = new Vector(0, 0);
     this._containers = [];
+
+    if (this._game.DEBUG) {
+      this.makeDebugShape;
+    }
+  }
+
+  // GETTERS & SETTERS
+
+  get isStopped() {
+    return this._isStopped;
+  }
+  set isStopped(state) {
+    this._isStopped = state;
+  }
+
+  get sprite() {
+    return this._sprite;
   }
 
   get value() {
@@ -16,60 +34,73 @@ class Block {
 
   addTo(container, u = new Vector(100, 0)) {
     this._containers.push(container);
-    container.addChild(this.sprite);
-    this.sprite.x = u.x;
-    this.sprite.y = u.y;
+    container.addChild(this._sprite);
+    this._sprite.x = u.x;
+    this._sprite.y = u.y;
     this._prevPosition.x = u.x;
     this._prevPosition.y = u.y;
   }
 
-  kill() {
-    for (const container of this._containers) {
-      container.removeChild(this.sprite);
-    }
-  }
+  // METHODS
 
-  moveX(pixels) {
-    this._prevPosition.x = this.sprite.x;
-    this.sprite.x += pixels;
-  }
-
-  moveY(pixels) {
-    this._prevPosition.y = this.sprite.y;
-    this.sprite.y += pixels;
-  }
-
-  checkBottom() {
-    if (this.sprite.y > this._game.h - this.sprite.height - 48) {
-      this.sprite.y = this._game.h - this.sprite.height - 48;
-      // this.sprite.y = this._prevPosition.y;
-
-      this.isStopped = true;
+  checkCollisions(block) {
+    if (block !== this) {
+      if (this._sprite.y > block.sprite.y - this._sprite.height && this._sprite.x == block.sprite.x) {
+        this._sprite.y = Math.floor(this._prevPosition.y);
+        this._isStopped = true;
+      }
     }
   }
 
   checkEdges() {
-    if (this.sprite.x < 0 || this.sprite.x + this.sprite.width > this._game.w) {
-      this.sprite.x = this._prevPosition.x;
+    if (this._sprite.x < 0 || this._sprite.x + this._sprite.width > this._game.w) {
+      this._sprite.x = this._prevPosition.x;
     }
   }
 
-  checkCollisions(block) {
-    if (block !== this) {
-      if (this.sprite.y > block.sprite.y - this.sprite.height && this.sprite.x == block.sprite.x) {
-        // this.sprite.y = block.sprite.y - this.sprite.height;
-        this.sprite.y = Math.floor(this._prevPosition.y);
-        this.isStopped = true;
-      }
+  checkGround() {
+    if (this._sprite.y > this._game.h - this._sprite.height - 48) {
+      this._sprite.y = this._game.h - this._sprite.height - 48;
+      // this.sprite.y = this._prevPosition.y;
+
+      this._isStopped = true;
     }
   }
-  /*
-blueprint = {
-  dropOffLocation: Vector(x, y),
-  blockTypes: [Block.CONST],
-}
-*/
+
+  kill() {
+    for (const container of this._containers) {
+      container.removeChild(this._sprite);
+    }
+  }
+
+  makeDebugShape() {
+    this._debugGraphics = new pixi.Graphics();
+    this._debugGraphics.lineStyle(1, 0xff0000, 1);
+    this._debugGraphics.beginFill(0x000000, 0);
+    this._debugGraphics.drawRect(this._sprite.x, this._sprite.y, this._sprite.width, this._sprite.height);
+    this._debugGraphics.endFill();
+    this._sprite.addChild(this._debugGraphics);
+  }
+
+  moveX(pixels) {
+    this._prevPosition.x = this._sprite.x;
+    this._sprite.x += pixels;
+  }
+
+  moveY(pixels) {
+    this._prevPosition.y = this._sprite.y;
+    this._sprite.y += pixels;
+  }
+
+  // STATIC METHODS
+
   static factory(blueprint) {
+    /*
+      blueprint = {
+      dropOffLocation: Vector(x, y),
+      blockTypes: [Block.CONST],
+      }
+    */
     const n = Math.floor(blueprint.blockTypes.length * Math.random());
     return blueprint.blockTypes[n];
   }
--- src/classes/Game.js
+++ src/classes/Game.js
@@ -1,183 +1,54 @@
 'use strict';
 
 const pixi = require('pixi.js');
+
 const Block = require('./Block');
 const Keyboard = require('./Keyboard');
 const KeyCodes = Keyboard.KeyCodes;
 const Loader = require('./Loader');
-const Vector = require('./Vector');
-
-// SCREENS
 const Screens = require('./Screens');
+const Vector = require('./Vector');
 
 class Game {
   constructor(options) {
-    this._options = options;
-    this._urls = this._options.assets;
-    this.w = this._options.width;
-    this.h = this._options.height;
-    this._keyboard;
-    this.app = new pixi.Application(this._options);
+    this.options = options;
+    this.urls = this.options.assets;
+    this.w = this.options.width;
+    this.h = this.options.height;
 
-    this._timeOver = 4000;
-    this._minGroupLengthGoal = 3;
-    this.speed = 6;
-    this._blockWidth = 48;
-    this._blockFactory = [];
-    this._staticBlocks = [];
-    this._dyingBlocks = [];
-    this._movingBlock;
-    this._prevPosition = new Vector();
-    this._rows;
-    this._gpsByRow = [];
-
+    this.app = new pixi.Application(this.options);
     this.app.renderer.backgroundColor = 0xeeeeee;
     this.app.renderer.autoResize = true;
-    // LAYERS
-    this.background = new pixi.Container();
-    this.background.name = 'background';
-    this.app.stage.addChild(this.background);
-    this.screen = new pixi.Container();
-    this.screen.name = 'screen';
-    this.screen.width = this.w;
-    this.screen.height = this.h;
-    this.screen.calculateBounds();
-    this.app.stage.addChild(this.screen);
-    this.hui = new pixi.Container();
-    this.screen.name = 'hui';
-    this.app.stage.addChild(this.hui);
 
-    this.app.ticker.add((dt) => {
-      this.loop(dt);
-    });
+    this.keyboard;
+    this.timeOver = 4000;
+    this.minGroupLengthGoal = 3;
+    this.speed = 6;
+    this.blockWidth = 48;
+    this.blockFactory = [];
+    this.staticBlocks = [];
+    this.dyingBlocks = [];
+    this.movingBlock;
+    this.prevPosition = new Vector();
+    this.rows;
+    this.gpsByRow = [];
 
-    this._makeKeyboard();
-
-    this.state = this.welcome;
-    new Loader(this._urls, this._dispatchSprites.bind(this));
-    document.body.appendChild(this.app.view);
+    this.makeLayers();
+    this.init();
+    this.DEBUG = false;
   }
 
-  loop(dt) {
-    if (this._timeOver < 0) {
-      this.state = this.gameover;
-    }
-    this.state(dt);
-    this._timeOver -= 1;
-  }
-
-  // STATES
-  welcome(dt) {
-    console.info('LOADING GAME...');
-    this._keyboard.space.press = () => {
-      this.state = this.preparePlay;
-      this.screen.removeChildren();
-      this.background.filters = [];
-    };
-    if (this.screen.children.length == 0) {
-      Screens.welcome(this.screen);
-    }
-    if (this.background.children.length > 0) {
-      this.background.filters = [new pixi.filters.BlurFilter(2, 4)];
-    }
-  }
-
-  preparePlay(dt) {
-    this._keyboard.left.press = () => {
-      this._movingBlock.moveX(-this._blockWidth);
-    };
-
-    this._keyboard.right.press = () => {
-      this._movingBlock.moveX(this._blockWidth);
-    };
-    this._keyboard.space.press = () => {};
-    this._rows = [];
-
-    this._resetRows();
-
-    this._newMovingBlock();
-    this.state = this.play;
-    console.info('GAME ON...');
-  }
-
-  play(dt) {
-    if (this._movingBlock !== undefined) {
-      this._movingBlock.moveY(this.speed * dt);
-
-      for (const block of this._staticBlocks) {
-        this._movingBlock.checkCollisions(block);
-      }
-      this._movingBlock.checkEdges();
-      this._movingBlock.checkBottom();
-
-      // Block stopped by hitting the bottom edge or the top of another bloc
-      if (this._movingBlock.isStopped) {
-        this._staticBlocks.push(this._movingBlock);
-        this._movingBlock = undefined;
-        this._checkLines();
-      }
-    } else {
-      this._moveLines(dt);
-    }
-  }
-
-  gameover(dt) {
-    console.info('GAME OVER');
-    this.app.ticker.stop();
-  }
-
-  _makeKeyboard() {
-    this._keyboard = new Keyboard({
-      space: KeyCodes.SPACE,
-      left: KeyCodes.LEFT,
-      right: KeyCodes.RIGHT,
-    });
-  }
-
-  _dispatchSprites(sprites) {
-    const pattBackground = /^..\/assets\/(background|bg)([a-z-_0-9])*.(png|jpg)$/i;
-    const pattBlock = /^..\/assets\/(block)([a-z-_0-9])*.(png|jpg)$/i;
-    for (const url in sprites) {
-      if (pattBackground.test(url)) {
-        this.background.addChild(sprites[url]);
-      } else if (pattBlock.test(url)) {
-        const block = new Block(sprites[url]);
-        this._blockFactory.push(block);
-      }
-    }
-
-    console.info('ASSETS LOADED...');
-  }
-
-  _newMovingBlock() {
-    const n = Math.floor(this._blockFactory.length * Math.random());
-    this._movingBlock = new Block(new pixi.Sprite(this._blockFactory[n].sprite.texture), this, n);
-    const u = new Vector((this.w - this._movingBlock.sprite.width) / 2, 0);
-    this._movingBlock.addTo(this.screen, u);
-  }
-
-  _resetRows() {
-    this._rows = [];
-    for (let n = 0; n < Math.floor(this.h / this._blockWidth); n++) {
-      this._rows[n] = [];
-      for (let k = 0; k < Math.floor(this.w / this._blockWidth); k++) {
-        this._rows[n][k] = { value: -1 };
-      }
-    }
-    this._dyingBlocks = [];
-  }
-
-  _checkLines() {
-    this._resetRows();
+  checkLines() {
+    this.resetRows();
     // Place block.value in a 2D array represneting the tetris grid
-    for (const block of this._staticBlocks) {
-      this._rows[Math.floor(block.sprite.y / this._blockWidth)][Math.floor(block.sprite.x / this._blockWidth)] = block;
+    for (const block of this.staticBlocks) {
+      this.rows[Math.floor(block.sprite.y / this.blockWidth)][Math.floor(block.sprite.x / this.blockWidth)] = block;
     }
 
     // Group similar adjacent block.value by rows
-    this._gpsByRow = [];
-    for (let k = this._rows.length - 1; k >= 0; k--) {
-      let row = this._rows[k];
+    this.gpsByRow = [];
+    for (let k = this.rows.length - 1; k >= 0; k--) {
+      let row = this.rows[k];
       let prevBlock = row[0];
       const gps = [];
       gps[0] = [prevBlock];
@@ -190,46 +61,107 @@ class Game {
         }
         prevBlock = block;
       }
-      this._gpsByRow.push(gps);
+      this.gpsByRow.push(gps);
     }
 
     // moving blocks from groups which length is > _minGroupLengthGoal
     // to _dyingBlocks
-    for (const row of this._gpsByRow) {
+    for (const row of this.gpsByRow) {
       for (const group of row) {
         if (group[0].value !== -1) {
-          if (group.length >= this._minGroupLengthGoal) {
+          if (group.length >= this.minGroupLengthGoal) {
             for (const block of group) {
-              this._dyingBlocks.push(block);
+              this.dyingBlocks.push(block);
             }
           }
         }
       }
     }
     // deleteing blocks from dyingBlocks
-    for (const block of this._dyingBlocks) {
+    for (const block of this.dyingBlocks) {
       block.kill();
-      this._staticBlocks.splice(this._staticBlocks.indexOf(block), 1);
+      this.staticBlocks.splice(this.staticBlocks.indexOf(block), 1);
     }
   }
 
-  _moveLines(dt) {
+  dispatchSprites(sprites) {
+    const pattBackground = /^..\/assets\/(background|bg)([a-z-_0-9])*.(png|jpg)$/i;
+    const pattBlock = /^..\/assets\/(block)([a-z-_0-9])*.(png|jpg)$/i;
+    for (const url in sprites) {
+      if (pattBackground.test(url)) {
+        this.background.addChild(sprites[url]);
+      } else if (pattBlock.test(url)) {
+        const block = new Block(sprites[url], this);
+        this.blockFactory.push(block);
+      }
+    }
+
+    console.info('ASSETS LOADED...');
+  }
+
+  init() {
+    this.app.ticker.add((dt) => {
+      this.loop(dt);
+    });
+
+    this.makeKeyboard();
+
+    new Loader(this.urls, this.dispatchSprites.bind(this));
+    document.body.appendChild(this.app.view);
+
+    this.state = this.welcome;
+  }
+
+  loop(dt) {
+    if (this.timeOver < 0) {
+      this.state = this.gameover;
+    }
+    this.state(dt);
+    this.timeOver -= 1;
+  }
+
+  makeKeyboard() {
+    this.keyboard = new Keyboard({
+      space: KeyCodes.SPACE,
+      left: KeyCodes.LEFT,
+      right: KeyCodes.RIGHT,
+    });
+  }
+
+  makeLayers() {
+    this.background = new pixi.Container();
+    this.background.name = 'background';
+    this.app.stage.addChild(this.background);
+
+    this.screen = new pixi.Container();
+    this.screen.name = 'screen';
+    this.screen.width = this.w;
+    this.screen.height = this.h;
+    this.screen.calculateBounds();
+    this.app.stage.addChild(this.screen);
+
+    this.hui = new pixi.Container();
+    this.screen.name = 'hui';
+    this.app.stage.addChild(this.hui);
+  }
+
+  moveLines(dt) {
     let nStopped = 0;
 
-    this._staticBlocks.sort(function (a, b) {
+    this.staticBlocks.sort(function (a, b) {
       return b.sprite.y - a.sprite.y;
     });
 
-    for (const block of this._staticBlocks) {
+    for (const block of this.staticBlocks) {
       block.isStopped = false;
     }
 
-    for (let n = 0; n < this._staticBlocks.length; n++) {
-      const block = this._staticBlocks[n];
+    for (let n = 0; n < this.staticBlocks.length; n++) {
+      const block = this.staticBlocks[n];
       if (!block.isStopped) {
         block.moveY(2 * this.speed * dt);
-        block.checkBottom();
-        for (const b of this._staticBlocks) {
+        block.checkGround();
+        for (const b of this.staticBlocks) {
           block.checkCollisions(b);
         }
         if (block.isStopped) {
@@ -238,8 +170,86 @@ class Game {
       }
     }
 
-    if (nStopped === this._staticBlocks.length) {
-      this._newMovingBlock();
+    if (nStopped === this.staticBlocks.length) {
+      this.newMovingBlock();
+    }
+  }
+
+  newMovingBlock() {
+    const n = Math.floor(this.blockFactory.length * Math.random());
+    this.movingBlock = new Block(new pixi.Sprite(this.blockFactory[n].sprite.texture), this, n);
+    const u = new Vector((this.w - this.movingBlock._sprite.width) / 2, 0);
+    this.movingBlock.addTo(this.screen, u);
+  }
+
+  resetRows() {
+    this.rows = [];
+    for (let n = 0; n < Math.floor(this.h / this.blockWidth); n++) {
+      this.rows[n] = [];
+      for (let k = 0; k < Math.floor(this.w / this.blockWidth); k++) {
+        this.rows[n][k] = { value: -1 };
+      }
+    }
+    this.dyingBlocks = [];
+  }
+
+  // STATES
+  gameover(dt) {
+    console.info('GAME OVER');
+    this.app.ticker.stop();
+  }
+
+  play(dt) {
+    if (this.movingBlock !== undefined) {
+      this.movingBlock.moveY(this.speed * dt);
+
+      for (const block of this.staticBlocks) {
+        this.movingBlock.checkCollisions(block);
+      }
+      this.movingBlock.checkEdges();
+      this.movingBlock.checkGround();
+
+      // Block stopped by hitting the bottom edge or the top of another bloc
+      if (this.movingBlock._isStopped) {
+        this.staticBlocks.push(this.movingBlock);
+        this.movingBlock = undefined;
+        this.checkLines();
+      }
+    } else {
+      this.moveLines(dt);
+    }
+  }
+
+  preparePlay(dt) {
+    this.keyboard.left.press = () => {
+      this.movingBlock.moveX(-this.blockWidth);
+    };
+
+    this.keyboard.right.press = () => {
+      this.movingBlock.moveX(this.blockWidth);
+    };
+    this.keyboard.space.press = () => {};
+    this.rows = [];
+
+    this.resetRows();
+
+    this.newMovingBlock();
+    this.state = this.play;
+    console.info('GAME ON...');
+  }
+
+  welcome(dt) {
+    console.info('LOADING GAME...');
+    this.keyboard.space.press = () => {
+      this.state = this.preparePlay;
+      this.screen.removeChildren();
+      this.background.filters = [];
+    };
+    if (this.screen.children.length == 0) {
+      Screens.welcome(this.screen);
+    }
+    if (this.background.children.length > 0) {
+      this.background.filters = [new pixi.filters.BlurFilter(2, 4)];
     }
   }
 }
